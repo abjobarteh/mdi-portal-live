77e45643b9adef49adb8e03f7f539f41300b9171
Baba Jasseh <babajasseh@gmail.com>
2024-08-08 16:19:21 +0000

    Bug Fixes

--- resources/js/api-config.js
+++ resources/js/api-config.js
@@ -1,4 +1,3 @@
-// api-config.js
-const baseURL = "http://localhost:8000"; // Replace with your API base URL
+const baseURL = "https://www.mdiportal.com"; // Replace with your API base URL
 
 export default baseURL;
